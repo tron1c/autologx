4ddbe6674e2a3a2681057ae290486cd286135a83
tron1c <alaali32@gmail.com>
2025-08-13 13:08:33 +0300

    fixed_login_logout

--- autologx/settings.py
+++ autologx/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'autologx.api', 
+    'autologx.api',
     'rest_framework',
     'widget_tweaks',
 ]
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'autologx.urls'
 
 TEMPLATES = [
     {
-       'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # --- Make sure this line includes your project's templates directory ---
         'DIRS': [os.path.join(BASE_DIR, 'templates')], # Should resolve to C:\Users\MY-PC\Desktop\unit 4\autologx\templates
         # ------------------------------------------------------------------------
@@ -72,6 +72,7 @@ TEMPLATES = [
         },
     },
 ]
+# ---> Added the missing closing square bracket and curly brace for TEMPLATES <---
 
 WSGI_APPLICATION = 'autologx.wsgi.application'
 
@@ -120,10 +121,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# --- Add this line to redirect users to the homepage after logout ---
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+# ---------------------------------------------------------------------
